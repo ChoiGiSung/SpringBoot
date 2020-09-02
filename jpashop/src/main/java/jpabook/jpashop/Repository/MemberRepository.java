d6c82ca7cf197f8bfc7b8945a82d8afafc50e2b6
ChoiGiSung <chlrltjd159@naver.com>
2020-09-02 18:38:43 +0900

    DB설정 및 기본jpa테스트

---
+++ jpashop/src/main/java/jpabook/jpashop/Repository/MemberRepository.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.Repository;
+
+import jpabook.jpashop.Entity.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+    //엔티티 매니저를 알아서 주입해준다
+
+    public Long save(Member member){
+        //저장하는 코드
+        em.persist(member);
+        return member.getId();
+        //커맨드와 쿼리를 불리해라 
+        //리턴값을 안남기는게 좋지만 혹시모를 나중을 위해 아이디를 리턴한다
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class,id);
+    }
+
+}
