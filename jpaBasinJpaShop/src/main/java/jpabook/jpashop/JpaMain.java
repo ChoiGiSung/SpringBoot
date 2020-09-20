997884fa99b69e01529fcf964d5a535e0b891508
ChoiGiSung <chlrltjd159@naver.com>
2020-09-20 17:38:23 +0900

    양방향 연관관계와 외래키 설정시 jpa의 방법

---
+++ jpaBasinJpaShop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args){
+        EntityManagerFactory emf= Persistence.createEntityManagerFactory("hello"); //persistance.xml에서 정의한 이름
+        EntityManager em=emf.createEntityManager();
+        EntityTransaction tx= em.getTransaction();
+
+        tx.begin();
+
+        try{
+            Order order=new Order();
+            order.addOrderItem(new OrderItem());
+            tx.commit();
+
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            emf.close();
+
+        }
+    }
+}
---
+++ jpaBasinJpaShop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,72 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="ORDERS")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name="ORDER_ID")
+    private Long id;
+
+//    @Column(name="MEMBER_ID") //db대로 설계했을 경우
+//    private Long memberId;
+    @JoinColumn(name ="MEMBER_ID") //단방향으로 설계
+    @ManyToOne
+    private Member member;
+
+    //하이버네이트는 자동 매핑되서 @TEMTIONAL을 안 써도 된다
+    private LocalDateTime orderDate;
+    
+    @Enumerated(EnumType.STRING) //이넘은 스트링으로 명시해줘라
+    private OrderStatus status;
+
+    @OneToMany(mappedBy = "order") //연관관계의 주인은 order야 //양방향이 아니라면 없어도 되는 것
+    private List<OrderItem> orderItems=new ArrayList<>();
+    //조회만을 위한 orderItems
+
+    public void addOrderItem(OrderItem orderItem) {
+        //연관관계 편의 메소드
+        //오더아이템리스트에 오더를 넣고 아이템에도 오더를 넣어준다
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+
+}
