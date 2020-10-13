a254a797c3e2a5ff94ee50c295624f972099b012
ChoiGiSung <chlrltjd159@naver.com>
2020-10-13 23:02:52 +0900

    O

--- springBoot_comunityBoard/demo/src/main/java/com/example/demo/web/domain/User.java
+++ springBoot_comunityBoard/demo/src/main/java/com/example/demo/web/domain/User.java
@@ -25,24 +25,30 @@ public class User {
     private String password;
     @Column
     private String email;
-//    @Column
-//    private String pincipal;
-//
-//    @Column
-//    @Enumerated(EnumType.STRING)
-//    private SocialType socialType;
 
     @Column
     private LocalDateTime createDate;
     @Column
     private LocalDateTime updateDate;
+    
+    //OAuth2 사용기
+    @Column
+    private String pincipal;
 
+    @Column
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    
     @Builder
-    public User(String name, String password, String email, LocalDateTime createDate, LocalDateTime updateDate) {
+    public User(Long idx, String name, String password, String email, LocalDateTime createDate, LocalDateTime updateDate, String pincipal, SocialType socialType) {
+        this.idx = idx;
         this.name = name;
         this.password = password;
         this.email = email;
         this.createDate = createDate;
         this.updateDate = updateDate;
+        this.pincipal = pincipal;
+        this.socialType = socialType;
     }
 }
--- springBoot_comunityBoard/demo/src/main/java/com/example/demo/web/domain/enums/SocialType.java
+++ springBoot_comunityBoard/demo/src/main/java/com/example/demo/web/domain/enums/SocialType.java
@@ -1,4 +1,26 @@
 package com.example.demo.web.domain.enums;
 
 public enum SocialType {
+    FACEBOOK("facebook"),
+    GOOGLE("google"),
+    KAKAO("kakao");
+
+    private final String ROLE_PREFIX = "ROLE_";
+    private String name;
+
+    SocialType(String name) {
+        this.name = name;
+    }
+
+    public String getROleType(){
+        return ROLE_PREFIX+name.toUpperCase();
+    }
+
+    public String getValue(){
+        return name;
+    }
+
+    public boolean isEquals(String authority){
+        return this.name.equals(authority);
+    }
 }
