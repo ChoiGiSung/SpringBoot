b5e79ea76d9b3389a9d31b7fe99743006474f880
ChoiGiSung <chlrltjd159@naver.com>
2020-10-20 22:11:45 +0900

    정리

--- src/main/java/showping/showpingsample/service/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package showping.showpingsample.service;
-
-import showping.showpingsample.Dto.ItemDto;
-import java.util.List;
-
-public interface ItemRepository {
-    ItemDto save(ItemDto itemDto); //상품등록
-    List<ItemDto> findAll(); //상품정보리스트
-}
