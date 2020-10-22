a906265ab1ec13e3ee7d93fe61017f0241396a71
ChoiGiSung <chlrltjd159@naver.com>
2020-10-22 23:30:53 +0900

    엔티티 설계

--- jpashopPractice/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashopPractice/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,16 +1,13 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
