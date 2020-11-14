6aeccdb47bdf03e7dea0562a1f0043a7377b572e
ChoiGiSung <chlrltjd159@naver.com>
2020-11-14 20:16:34 +0900

    쿼리dsl

---
+++ querydsl/querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,36 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+
+import javax.persistence.EntityManager;
+
+@Transactional
+@SpringBootTest
+class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager entityManager;
+
+	@Test
+	void contextLoads() {
+		Member member=new Member();
+		entityManager.persist(member);
+
+		JPAQueryFactory queryFactory=new JPAQueryFactory(entityManager);
+		QMember qMember = new QMember("m");
+
+		Member result = queryFactory
+				.selectFrom(qMember)
+				.fetchOne();
+
+		Assertions.assertEquals(result,member);
+	}
+
+}
