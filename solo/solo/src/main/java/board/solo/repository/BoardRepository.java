0fb15ae013e18304bfe5ed6cb6c17e3b49e6c33f
ChoiGiSung <chlrltjd159@naver.com>
2020-11-24 21:41:52 +0900

    스프링 데이터 jpa로 페이징

--- solo/solo/src/main/java/board/solo/repository/BoardRepository.java
+++ solo/solo/src/main/java/board/solo/repository/BoardRepository.java
@@ -1,26 +1,44 @@
 package board.solo.repository;
 
 import board.solo.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
-public class BoardRepository {
+public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    private EntityManager entityManager;
-    public BoardRepository(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
 
-    public Long save(Board board){
-        entityManager.persist(board);
-        return board.getId();
-    }
+  //  private EntityManager entityManager;
+//    public BoardRepository(EntityManager entityManager) {
+//        this.entityManager = entityManager;
+//    }
+//
+//    public Long save(Board board){
+//        entityManager.persist(board);
+//        return board.getId();
+//    }
+//
+//    public Board find(Long id){
+//        Board findBoard = entityManager.find(Board.class, id);
+//        return findBoard;
+//    }
+//
+//    //페이징
+//    public Page<Board> findAll(Pageable pageable) {
+//        List<Board> select_b_from_board_m = entityManager.createQuery("select b from Board m", Board.class)
+//                .setFirstResult(pageable.getPageNumber())
+//                .setMaxResults(pageable.getPageSize())
+//                .getResultList();
+//
+//    }
 
-    public Board find(Long id){
-        Board findBoard = entityManager.find(Board.class, id);
-        return findBoard;
-    }
+
+
 
 }
--- solo/solo/src/main/java/board/solo/service/BoardService.java
+++ solo/solo/src/main/java/board/solo/service/BoardService.java
@@ -2,6 +2,9 @@ package board.solo.service;
 
 import board.solo.entity.Board;
 import board.solo.repository.BoardRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,9 +18,16 @@ public class BoardService {
         this.boardRepository = boardRepository;
     }
 
+    //글쓰기
     public Long wirtePost(String title,String content){
         Board board=new Board(title,content);
-        Long findId = boardRepository.save(board);
-        return findId;
+        Long id = boardRepository.save(board).getId();
+        return id;
+    }
+
+    //페이징
+    public Page<Board> findBoardList(Pageable pageable){
+        pageable = PageRequest.of(pageable.getPageNumber()<=0 ?0:pageable.getPageNumber()-1, pageable.getPageSize());
+        return boardRepository.findAll(pageable);
     }
 }
